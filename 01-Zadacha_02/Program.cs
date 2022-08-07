1631d37beb4ace7d1761936ebc7814460b0d10c1
Andrey-Rya <dinamit2007@mail.ru>
2022-08-08 02:59:42 +0700

    add 01-Zadacha_02/Program.cs

---
+++ 01-Zadacha_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа на входе имеет 2 числа а на выходе показывает числа max и min
+Console.WriteLine("Введите число a: ");// вводим число А
+int numberA = Convert.ToInt32(Console.ReadLine()); // запоминаем А
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());// запоминаем B
+if (numberA > numberB)
+{
+    int max = numberA;
+    int min = numberB;
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    int max = numberB;
+    int min = numberA;
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
