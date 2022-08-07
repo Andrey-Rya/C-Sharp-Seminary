14b3f4fc998670de97fb727a0a749c08904ae3ae
Andrey-Rya <dinamit2007@mail.ru>
2022-08-08 01:39:35 +0700

    add new folder 01-Zadacha_01

---
+++ 01-Zadacha_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа проверки является ли первое число квадратом второго
+
+Console.WriteLine("Введите число A: ");// вводим число А
+int numberA = Convert.ToInt32(Console.ReadLine()); // запоминаем А
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());// запоминаем B
+if ((numberA / numberB) == numberB)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
