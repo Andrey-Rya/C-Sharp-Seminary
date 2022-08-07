89ed69f087527111d6cc816a246325d9d4344474
Andrey-Rya <dinamit2007@mail.ru>
2022-08-08 02:44:25 +0700

    add new file Progra.cs

--- 01-Zadacha_03/Program.cs
+++ 01-Zadacha_03/Program.cs
@@ -33,3 +33,35 @@ else if (number_ned > 7)
 {
     Console.WriteLine("Нет такого номера в неделе");
 }
+
+// другой вариант решения вывода дня недели по заданному номеру
+// Console.WriteLine("Введите номер дня недели (от 1 до 7): ");// вводим № дня недели
+//int number = Convert.ToInt32(Console.ReadLine()); // запоминаем № дня недели
+
+//switch (number)
+//{
+  //  case 1:
+  //    Console.WriteLine("Понедельник");
+  //    break;
+  //  case 2:
+  //    Console.WriteLine("Вторник");
+  //    break;
+  //  case 3:
+  //    Console.WriteLine("Среда");
+  //    break;
+  //  case 4:
+  //    Console.WriteLine("Четверг");
+  //    break;
+  //  case 5:
+  //    Console.WriteLine("Пятница");
+  //    break;
+  //  case 6:
+  //    Console.WriteLine("Суббота");
+  //    break;
+  //  case 7:
+  //    Console.WriteLine("Воскресенье");
+  //    break;
+  //  default:
+  //    Console.WriteLine("Нет такого дня");
+  //    break;
+//}
