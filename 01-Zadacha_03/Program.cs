699d515145317591279587339a99233f69e12298
Andrey-Rya <dinamit2007@mail.ru>
2022-08-08 02:17:39 +0700

    add folders 01-Zadacha_03

---
+++ 01-Zadacha_03/Program.cs
@@ -0,0 +1,35 @@
+﻿// программа выдаёт название дня недели по заданному номеру
+Console.WriteLine("Введите номер дня недели (от 1 до 7): ");// вводим № дня недели
+int number_ned = Convert.ToInt32(Console.ReadLine()); // запоминаем № дня недели
+if (number_ned == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (number_ned == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (number_ned == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (number_ned == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (number_ned == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (number_ned == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (number_ned == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else if (number_ned > 7)
+{
+    Console.WriteLine("Нет такого номера в неделе");
+}
