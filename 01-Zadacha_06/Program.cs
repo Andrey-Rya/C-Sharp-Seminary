c74d9a2e4d68c8a91f27d47b60771b5a416efb87
Andrey-Rya <dinamit2007@mail.ru>
2022-08-08 13:26:41 +0700

    add 01-Zadacha_06

---
+++ 01-Zadacha_06/Program.cs
@@ -0,0 +1,15 @@
+﻿// проверяем четное это число или нет?
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = N % 2;
+Console.WriteLine("***");
+// проверяем условие на четность иначе на else
+if (A == 0)
+{
+    Console.WriteLine("Да - это чётное число"); // % - показывает остаток от деления на 2
+}
+else
+{
+    Console.WriteLine("Нет = это нечётное число");
+}
+
