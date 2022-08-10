0a896abf24c2d1587fe40df3fd7a3c4e30657951
Andrey-Rya <dinamit2007@mail.ru>
2022-08-10 22:35:13 +0700

    git add 02-Zadacha_10/Program.cs

---
+++ 02-Zadacha_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine($"Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>=100 & N<1000)
+{
+    int N1 = N / 10;
+    int N2 = N1 % 10;
+    Console.WriteLine(N2);
+      
+}
+else
+{
+    Console.WriteLine($"Ошибка! Повторите ввод!");
+}
+    
+
+
