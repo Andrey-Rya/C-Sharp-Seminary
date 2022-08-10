f08fa3c92fec1454bd4cf97f116a02de15bcdd20
Andrey-Rya <dinamit2007@mail.ru>
2022-08-10 22:07:47 +0700

    add 02-Zadacha_11/Program.cs

---
+++ 02-Zadacha_11/Program.cs
@@ -0,0 +1,6 @@
+﻿// программа выводит случайное трехзначное число и удаляет вторую букву этого числа
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Наше случайное число = {number}");
+int numberA = number / 100;
+int numberC = number % 10;
+Console.WriteLine(numberA * 10 + numberC);
