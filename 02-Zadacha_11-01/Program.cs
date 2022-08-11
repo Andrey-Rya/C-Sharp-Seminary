283c9ad9e20637d80165261869cbf6aa1b532037
Andrey-Rya <dinamit2007@mail.ru>
2022-08-11 22:16:08 +0700

    add 02-Zadacha_11-01 - работаем со строкой

---
+++ 02-Zadacha_11-01/Program.cs
@@ -0,0 +1,6 @@
+﻿// второй вариант решения вывода первой и последней цифры случайного трехзначного числа
+string s = new Random().Next(100, 1000).ToString(); // конвертируем рандом число в строку
+Console.WriteLine($"Для проверки: {s}");
+int first = int.Parse(s[0].ToString()); // находим в строке первое чиcло (нулевой эл-т)
+int last = int.Parse(s[s.Length - 1].ToString()); // // находим в строке последнее число [s.Length - 1]
+Console.WriteLine($"{first}{last}"); // печатаем их в терминале
