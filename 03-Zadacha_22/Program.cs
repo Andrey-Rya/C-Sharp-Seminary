526906677cabc87c5a221c463fca223a7c8cb869
Andrey-Rya <dinamit2007@mail.ru>
2022-08-16 22:42:16 +0700

    add 03-Zadacha_22/возведение в квадрат

---
+++ 03-Zadacha_22/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход число (N) от 1 до N
+Console.WriteLine("Введите любое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count >=1 && count <= N)
+{
+    Console.WriteLine(Math.Pow((count), 2));
+    count++;
+}
+
