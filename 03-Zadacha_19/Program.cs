af969f40bee549da19956829f7074db3b5a32cf6
Andrey-Rya <dinamit2007@mail.ru>
2022-08-18 11:46:48 +0700

    add 03-Zadacha_19/Проверка числа на зеркальность (палиндром

--- 03-Zadacha_19/Program.cs
+++ 03-Zadacha_19/Program.cs
@@ -3,7 +3,6 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-// string[] arr = { "x>0;y>0", "x<0;y>0", "x<0;y<0", "x>0;y<0" }; // записываем все условия в массив
 if (number >= 10000 && number < 100000) // если number >=10000 и <=100 000 то делаем ниже
 {
     Console.WriteLine();
@@ -20,15 +19,3 @@ else
 {
     Console.WriteLine("Вы ввели не пятизначное число. Повторите ввод!");
 }
-
-
-// второй вариант решения вывода первой и последней цифры 
-// случайного трехзначного числа
-// string s = new string().Next(100, 1000).ToString(); // конвертируем рандом число в строку
-// Console.WriteLine($"Для проверки: {s}");
-// int first = int.Parse(s[0].ToString()); // находим в строке первое чиcло (нулевой эл-т)
-// int last = int.Parse(s[s.Length - 1].ToString()); // // находим в строке последнее число [s.Length - 1]
-// Console.WriteLine($"{first}{last}"); // печатаем их в терминале
-
-
-// Console.WriteLine("Hello, World!");
