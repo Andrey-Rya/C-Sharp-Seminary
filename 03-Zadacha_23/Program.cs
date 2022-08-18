e6d612ac4f70c4bd1657442d332a62723d6296bf
Andrey-Rya <dinamit2007@mail.ru>
2022-08-18 10:56:02 +0700

    add 03-Zadacha_23/Выдает таблицу кубов чисел

--- 03-Zadacha_23/Program.cs
+++ 03-Zadacha_23/Program.cs
@@ -2,7 +2,7 @@
 // число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Console.WriteLine("Введите любое число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+Console.WriteLine(); // просто разьединил строку ))
 for (int count = 1; count <= N; count++)
 {
 	Console.WriteLine(Math.Pow((count), 3));
