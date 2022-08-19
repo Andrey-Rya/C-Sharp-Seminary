1d59e2178af59627c583e9eafc67d2912ae5eab3
Andrey-Rya <dinamit2007@mail.ru>
2022-08-19 10:35:05 +0700

    add 04-Zadacha_24-1/вывод суммы от 1 до N (функция)

---
+++ 04-Zadacha_24-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа принимает на вход число А и выдает 
+// сумму чисел от 1 до А (7->28, 4->10, 8->36)
+// ИСПОЛЬЗУЕМ ФУНКЦИЮ
+int cycle(int a)
+{
+  int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+	sum = sum + i;
+    } 
+    return sum; 
+}
+
+Console.Write("Ввод числа А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = cycle(number); // запоминаем значение sum 
+Console.Write($"Результат работы программы = {sum}");
+// Console.Write($"Результат работы программы = {cycle(number)}"); второй вариант вывода
