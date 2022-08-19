a33d8caa2156d31ede6a0af0b11546b20146d7ea
Andrey-Rya <dinamit2007@mail.ru>
2022-08-19 12:04:34 +0700

    add 04-Zadacha_26/показывает количество цифр в числе

---
+++ 04-Zadacha_26/Program.cs
@@ -0,0 +1,29 @@
+﻿// программа принимает на вход число и выдает количество цифр в нём
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 0;
+// while (number >0)
+// {
+//     number = number / 10;
+//     i += 1;
+// }
+// Console.WriteLine(i);
+
+
+// решаем задачу через цикл
+
+Console.Write("Введите число: ");
+int cycle(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+	number = number / 10;
+    sum += 1;
+    } 
+    return sum;  
+}
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результат работы программы = {cycle(number)}");
+
