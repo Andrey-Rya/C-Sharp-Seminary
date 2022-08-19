b08fb76b354aac420f8ed45bdb402ba7557723b1
Andrey-Rya <dinamit2007@mail.ru>
2022-08-19 09:48:30 +0700

    add 04-Zadacha_24/вывод суммы от 1 до N

---
+++ 04-Zadacha_24/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа принимает на вход число А и выдает 
+// сумму чисел от 1 до А (7->28, 4->10, 8->36)
+
+Console.Write("Ввод числа А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+	sum = sum + i;
+}
+Console.Write($"Результат работы программы = {sum}");
