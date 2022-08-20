d951e6989b8d5be03106706b35ff5d366b1d5353
Andrey-Rya <dinamit2007@mail.ru>
2022-08-20 09:38:31 +0700

    add 04-Zadacha_26-1/решение предыдущей задачи через строку

---
+++ 04-Zadacha_26-1/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа принимает на вход число и выдает количество цифр в нём
+// решение через строку
+Console.Clear();
+Console.Write("Введите число: "); // ввод числа  в строку
+int n = Convert.ToInt32(Console.ReadLine()); // переводим строку в число 
+n = Math.Abs(n); // проверка на отрицательные числа
+string s = Convert.ToString(n);
+Console.Write(s.Length);
+
