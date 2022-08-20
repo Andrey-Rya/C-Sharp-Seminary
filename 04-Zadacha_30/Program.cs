56efc8c827cddd87ad9d16379151eb541647d983
Andrey-Rya <dinamit2007@mail.ru>
2022-08-20 13:16:44 +0700

    add 04-Zadacha_30/вывод массива из 8-ми случ эл-тов [0 и1]

---
+++ 04-Zadacha_30/Program.cs
@@ -0,0 +1,25 @@
+﻿// программа выводит массив из 8 элементов, заполненный нулями
+// и единицами в случайном порядке.[1,0,1,1,0,1,0,0]
+
+// for (int i = 0; i <= 7; i++)
+//     {
+//         int rnd = new Random().Next(0, 2);
+//         Console.Write($"{rnd},");
+//     }
+  
+// второй вариант решения этой задачи
+Console.Clear();
+int N = 8; // задаем кол-во элементов массива
+int min = 0; // задали нулевой элемент
+int max = 1; // задали элемент 1
+int [] Array(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+Console.WriteLine();
+Console.WriteLine('[' + string.Join(", ", Array(N,min,max))+']');// разделили строку и  вывели массив
