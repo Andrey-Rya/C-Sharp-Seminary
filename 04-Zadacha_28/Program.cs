77808bcdb2ea05cb599007a91bd11b6eccee5bba
Andrey-Rya <dinamit2007@mail.ru>
2022-08-20 10:52:17 +0700

    add 04-Zadacha_28/выводим произведение чисел от 1 до N

---
+++ 04-Zadacha_28/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает на вход число N и выдаёт произведение 
+// чисел от 1 до N
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for (int i = 1; i <= n; i += 1)
+{
+    sum = sum * i;
+}
+Console.WriteLine(sum);
