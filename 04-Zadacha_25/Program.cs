1554ed544df59c1c735f66a4bdcfe84eb7b7a0e7
Andrey-Rya <dinamit2007@mail.ru>
2022-08-21 10:11:43 +0700

    add 04-Zadacha_25/вводим 2 числа и возводим 1-е число в степень 2-го

---
+++ 04-Zadacha_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// цикл принимает на вход 2 числа (А и В) и возводит число А в 
+// натуральную степень В
+Console.Clear();
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Exp(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int degree = Exp(a, b);
+Console.WriteLine($"Число {a} в степени {b} = " + degree);
