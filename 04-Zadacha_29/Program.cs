5b1cf41206104679cefdb881bf9f47a0ba791d55
Andrey-Rya <dinamit2007@mail.ru>
2022-08-22 15:15:33 +0700

    add 04-Zadacha_29/задаем и выводим на экран массив из 8-ми элементов

---
+++ 04-Zadacha_29/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear(); // очистка консоли
+int N = 8; // задаем кол-во элементов массива
+int[] Array = new int[N]; // создали массив из 8-ми симв
+Console.WriteLine("Введите массив из 8 цифр: ");
+
+for (int i = 0; i < N; i++) // цикл ввода массива с клавиатуры
+{
+	Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(); // просто пробел
+Console.Write("Вы ввели этот массив: ");
+
+for (int i = 0; i < N; i++) // вывод массива в терминал
+{
+	Console.Write("{0}, ", Array[i]);
+}
