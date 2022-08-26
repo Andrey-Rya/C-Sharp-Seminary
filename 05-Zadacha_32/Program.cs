cd4725ff71ba511383696d114d15ae1f39d6be18
Andrey-Rya <dinamit2007@mail.ru>
2022-08-26 09:19:20 +0700

    Задача_32/меняем знаки чисел в рандомном массиве

---
+++ 05-Zadacha_32/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа замены положит элементов массива на отрицательные
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+int[] array = new int[4]; // задали массив из 4-х симв
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-9, 10);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1; // умножаем каждый символ на -1
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
