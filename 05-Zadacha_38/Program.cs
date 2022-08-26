92a0d996226eda11cc20ef7303843dad18dc4e74
Andrey-Rya <dinamit2007@mail.ru>
2022-08-26 15:44:57 +0700

    Задача_38/зададим массив чисел и найдем разницу между макс и мин элементами

---
+++ 05-Zadacha_38/Program.cs
@@ -0,0 +1,38 @@
+﻿// Зададим с клавиатуры массив (double) чисел и найдем
+// разницу между макс и мин элементом массива
+
+/* ввод элементов массива с клавиатуры */
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[num];
+for (int count = 0; count < num; count++)
+{
+    Console.Write($"Введите {count + 1}-й элемент массива: ");
+    array[count] = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine();
+
+/* вывод введенного массива */
+Console.Write($"Вы ввели этот массив: ");
+Console.Write('[' + string.Join(", ", array) + ']');
+Console.Write($" из {array.Length} чисел.");
+Console.WriteLine();
+
+/* находим макс и мин массива */
+double min = array[0];
+double max = 0;
+
+for (int z = 0; z < array.Length; z++)
+{
+    if (array[z] > max)
+    {
+        max = array[z];
+    }
+    if (array[z] < min)
+    {
+        min = array[z];
+    }
+}
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
