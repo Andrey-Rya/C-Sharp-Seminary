6c92f0642efb23f9d968aab51540fc12ce2ac0ed
Andrey-Rya <dinamit2007@mail.ru>
2022-08-26 10:15:03 +0700

    Задача_34/cчитаем количество положит чисел в рандомном массиве

---
+++ 05-Zadacha_34/Program.cs
@@ -0,0 +1,22 @@
+﻿// задать массив с положительными трехзначными числами
+// подсчитать количество четных чисел в массиве
+
+Console.Clear();
+int[] array = new int[4]; // задали массив из 4-х симв
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+}
+Console.Write("Наш случайный массив: ");
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+ {
+   if (array[i] % 2 == 0)
+   {
+        counter ++;
+   }
+ }
+    Console.WriteLine($"Количество четных чисел в массиве = " + counter);
+
