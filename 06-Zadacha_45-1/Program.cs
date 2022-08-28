88887bb77d4dd901501a1a4e3894fba98e7fc828
Andrey-Rya <dinamit2007@mail.ru>
2022-08-28 21:06:48 +0700

    Задача_45/находим произведение от 1 до N через рекурсию

---
+++ 06-Zadacha_45-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа принимает на вход число N и выдаёт произведение
+// чисел от 1 до N. (использовать рекурсию)
+
+Console.Clear();
+Console.Write("Введите значение N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+double Result(int n) 
+{
+    // 5 = 1*2*3*4*5 = 120
+    // 4 = 1*2*3*4 = 24
+    // 3 = 1*2*3 = 6
+    if (n == 1) return 1;
+    else return n * Result(n-1);
+}
+Console.WriteLine($"Произведение чисел от 1 до {number} = {Result(number)}");
