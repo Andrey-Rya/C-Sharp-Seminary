ac44d0e52bb6e32393347bbb3750dc3acab64a25
Andrey-Rya <dinamit2007@mail.ru>
2022-08-29 09:26:48 +0700

    Задача_46/задаем случайный массив m x n, заполненный случайными целыми числами

---
+++ 07-Zadacha_46/Program.cs
@@ -0,0 +1,22 @@
+﻿// зададим двумерный массив размером m x n, заполненный 
+// случайными целыми числами
+
+
+Console.Clear();
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.Write("Введите m:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n]; // задаем двумерный массив
+
+for (int i = 0; i < m; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		matrix[i, j] = new Random().Next(-100, 100); // заполняем строку
+		Console.Write(matrix[i, j] + " ");
+	}
+	Console.WriteLine();
+}
