7acb43c66a8d9e021c7b90cf2ce99f2ca724ef4e
Andrey-Rya <dinamit2007@mail.ru>
2022-08-29 09:55:55 +0700

    Задача_47/задали двумерный массив m x n и заполнили его случайными вещественными числами

---
+++ 07-Zadacha_47/Program.cs
@@ -0,0 +1,24 @@
+﻿// зададим двумерный массив размером m x n, заполненный 
+// случайными вещественными числами
+
+
+Console.Clear();
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.WriteLine();
+Console.Write("Введите количество строк m:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов  n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n]; // задаем двумерный массив
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100))/10; // заполняем строку
+        Console.Write(matrix[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
