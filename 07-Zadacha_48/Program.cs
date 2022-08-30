72e8b6294e4852bdceff7330882c90dd9b312d24
Andrey-Rya <dinamit2007@mail.ru>
2022-08-30 11:12:00 +0700

    Задача_48/задали двухмерный массив m x n и заполнили его по формуле А=m+n

--- 07-Zadacha_48/Program.cs
+++ 07-Zadacha_48/Program.cs
@@ -1,6 +1,27 @@
 ﻿// зададим двумерный массив размером m x n. Каждый элемент 
 // массива находим по формуле А = m + n
 
+// === мой первый вариант решения задачи === //
+// Console.Clear();
+// Console.WriteLine("Задайте двумерный массив m x n");
+// Console.WriteLine();
+// Console.Write("Введите количество строк m:  ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов  n:  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] table = new int[m, n]; // задаем двумерный массив
+
+// for (int rows = 0; rows < m; rows++) // счетчик строк
+// {
+//     for (int columns = 0; columns < n; columns++) // счетчик столбцов
+//     {
+//         Console.Write($"  {table[rows, columns]=rows+columns}  ");
+//     }
+//     Console.WriteLine();
+// }
+
+// второй вариант решения 
 Console.Clear();
 Console.WriteLine("Задайте двумерный массив m x n");
 Console.WriteLine();
@@ -15,7 +36,8 @@ for (int rows = 0; rows < m; rows++) // счетчик строк
 {
     for (int columns = 0; columns < n; columns++) // счетчик столбцов
     {
-        Console.Write($"  {table[rows, columns]=rows+columns}  ");
+        table[rows, columns] = rows + columns;
+        Console.Write(table[rows, columns] + "  ");
     }
     Console.WriteLine();
 }
