713d50f957e11644c07e7c58759f3cc01e17b7d2
Andrey-Rya <dinamit2007@mail.ru>
2022-09-05 12:17:25 +0700

    Задача_65/Рекурсия. Задайте M и N и выведите все натуральные числа от М до N

---
+++ 09-Zadacha_65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N с помощью рекурсии
+
+Console.Clear();
+int m = InputNum("Введите m: ");
+int n = InputNum("Введите n: ");
+
+void PrintNumbers(int number, int n)
+{
+    if (number == n-1)
+    {
+        return;
+    }
+    PrintNumbers(number - 1, n);
+    Console.Write($" {number}, ");
+}
+
+Console.Write($"\nM={m}; N={n} -> [");
+
+int InputNum(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+PrintNumbers(n, m);
+Console.Write("]");
+
