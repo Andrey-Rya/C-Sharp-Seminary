aa6fb86c63be1730f8544474a399852d28e01271
Andrey-Rya <dinamit2007@mail.ru>
2022-09-05 12:37:15 +0700

    Задача_64/Рекурсия. Задайте N и выведите все натуральные числа от N до 1

---
+++ 09-Zadacha_64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от N до 1 с помощью рекурсии
+
+Console.Clear();
+int n = InputNum("Введите N: ");
+
+void PrintNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    Console.Write($" {number}, ");
+    PrintNumbers(number - 1);
+    
+}
+
+Console.Write($"\nN={n} -> [");
+
+int InputNum(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+PrintNumbers(n);
+Console.Write("]");
